c1e55acdc356c9eb041b6f7ff798721da929b28f
Restyled.io <commits@restyled.io>
2020-02-13 07:57:46 +0000

    Restyled by shellharden

--- src/locale/genpot.sh
+++ src/locale/genpot.sh
@@ -4,6 +4,6 @@
 PROJECT_ID_VERSION="woeusb"
 REPORT_MSGID_BUGS_TO="https://github.com/slacka/WoeUSB/issues"
 
-xgettext -C -k_ --package-name=$PROJECT_ID_VERSION --msgid-bugs-address=$REPORT_MSGID_BUGS_TO \
+xgettext -C -k_ --package-name="$PROJECT_ID_VERSION" --msgid-bugs-address="$REPORT_MSGID_BUGS_TO" \
   -o woeusb.pot --foreign-user --from-code=UTF-8 \
-  $(find .. -name '*.cpp' -or -name "*.hpp" -or -name "*.c" -or -name "*.h")
+  "$(find .. -name '*.cpp' -or -name "*.hpp" -or -name "*.c" -or -name "*.h")"
